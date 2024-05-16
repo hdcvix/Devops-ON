f95459a436e9099530eb7bfd910b7b05e6dbc922
Henrique Dias <67007134+hdcvix@users.noreply.github.com>
2024-05-16 18:05:07 -0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,52 +16,50 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	}
-       
-	@GetMapping("/")
-        public String healthCheck(){
-                return "HEALTH CHECK OK!";
-        }
+    @GetMapping("/")
+    public String healthCheck() {
+        return "HEALTH CHECK OK!";
+    }
 
-	@GetMapping("/secured")
-	public Object secured(@LoggedInUser AppUser appUser){
-		return appUser.getUser();
-	}
+    @GetMapping("/secured")
+    public Object secured(@LoggedInUser AppUser appUser) {
+        return appUser.getUser();
+    }
 
-	@GetMapping("/secured-admin")
-	@PreAuthorize("hasRole('ROLE_admin')")
-	public String securedAdmin(){
-		return "Only admin can see  this";
-	}
-	
-	@GetMapping("/public")
-	public String pub(){
-		
-		return "This is public endpoint";
-	}
+    @GetMapping("/secured-admin")
+    @PreAuthorize("hasRole('ROLE_admin')")
+    public String securedAdmin() {
+        return "Only admin can see this";
+    }
 
-	@GetMapping("/what-is-the-time")
-	String time(){
-		return new Date().toString();
-	}
-	
-	@GetMapping("/devops")
-	String turma(){
-		return "Zumbi";
-	}
+    @GetMapping("/public")
+    public String pub() {
+        return "This is public endpoint";
+    }
 
+    @GetMapping("/what-is-the-time")
+    public String time() {
+        return new Date().toString();
+    }
 
-    	@GetMapping("/image")
-   	@ResponseBody
-    	public String getImage() {
-        // Aqui você pode retornar o caminho para a imagem ou o conteúdo da imagem
-        // por meio de um objeto de byte ou base64.
-        // Por exemplo:
-        // return "<img src='/caminho/para/sua/imagem.jpg'>";
+    @GetMapping("/devops")
+    public String turma() {
+        return "Zumbi";
+    }
+
+    @GetMapping("/image")
+    @ResponseBody
+    public String getImage() {
         return "<img src='https://learn.microsoft.com/pt-br/shows/devops-lab/media/devopslab-titlecard.png'>";
     }
 
-}	
+    @GetMapping("/gif")
+    @ResponseBody
+    public String getGif() {
+        return "<img src='https://media.giphy.com/media/3oEjI5VtIhHvK37WYo/giphy.gif' alt='Animated GIF'>";
+    }
+}
